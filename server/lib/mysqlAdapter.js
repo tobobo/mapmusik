05af6a408c24f7c18cf80c58618b40da86377679
Tobias Butler <tobias@change.org>
2019-01-03 23:10:53 -0800

    add updateVideo

--- server/lib/mysqlAdapter.js
+++ server/lib/mysqlAdapter.js
@@ -2,7 +2,12 @@ module.exports = connection => {
   const getVideos = () =>
     connection.query('select * from videos order by created_at desc limit 100');
   const createVideo = video =>
-    connection.query(`insert into videos SET ?`, { ...{ created_at: new Date() }, video });
+    connection.query(`insert into videos SET ?`, { created_at: new Date(), ...video });
+  const updateVideo = (id, video) =>
+    connection.query(`update videos SET ? where id = ?`, [
+      { updated_at: new Date(), ...video },
+      id,
+    ]);
 
-  return { getVideos, createVideo };
+  return { getVideos, createVideo, updateVideo };
 };
