61494752966c5dc65603e912057c42654c9994e8
Tobias Butler <tobias@change.org>
2019-01-05 16:02:22 -0800

    allow extensions in imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   rules: {
     'prettier/prettier': ['error'],
     'no-console': ['warn'],
-    "import/no-extraneous-dependencies": ["error", { 'devDependencies': ['./server/startDevServer.mjs'] }]
+    'import/no-extraneous-dependencies': ['error', { 'devDependencies': ['./server/startDevServer.mjs'] }],
+    'import/extensions': ['never'],
   },
 };
