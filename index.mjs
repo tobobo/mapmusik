0707aecce62ea6d75a366241a9ef194111a85a68
Tobias Butler <tobias@change.org>
2019-02-06 20:58:35 -0800

    actually start the serverr

--- index.mjs
+++ index.mjs
@@ -1,9 +1,10 @@
 import mysql from 'promise-mysql';
 import createMysqlAdapter from './server/lib/mysqlAdapter.mjs';
 import startDevServer from './server/startDevServer.mjs';
+import startDevServer from './server/startProdServer.mjs';
 import config from './config/server.mjs';
 
-const startServer = process.env.NODE_ENV === 'production' ? null : startDevServer;
+const startServer = process.env.NODE_ENV === 'production' ? startProdServer : startDevServer;
 
 const run = async () => {
   const connection = await mysql.createConnection(config.mysql);
