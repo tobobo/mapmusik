5dcca4103074547656916ddba3dc8d153cb4aec8
Tobias Butler <tobias@change.org>
2019-02-19 22:18:28 -0800

    run for just one video for now

--- createPreviewUrls.mjs
+++ createPreviewUrls.mjs
@@ -12,7 +12,7 @@ const main = async () => {
   const connection = await mysql.createConnection(config.mysql);
   const { updateVideo } = createMysqlAdapter(connection);
   const uploadsWithVideoIds = await connection.query(
-    'select uploads.url as uploadUrl, videos.id as videoId from uploads join videos on uploads.id = videos.upload_id'
+    'select uploads.url as uploadUrl, videos.id as videoId from uploads join videos on uploads.id = videos.upload_id limit 1'
   );
   await Promise.all(
     fp.map(async ({ uploadUrl, videoId }) => {
